3404074c1d66724082b3ec03865e5b36152203fb
Thomas Briot <thomas.briot82@gmail.com>
2023-09-24 12:06:57 -0400

    Add git dotfile

--- uninstall.sh
+++ uninstall.sh
@@ -4,3 +4,9 @@
 chsh /bin/bash
 apt purge zsh
 rm -fr ~/powerlevel10k
+
+# Delete dotfiles
+rm -f $HOME/.gitconfig
+rm -fr $XDG_CONFIG_HOME/git
+rm -fr $XDG_CONFIG_HOME/p10k
+rm -fr $XDG_CONFIG_HOME/zsh
