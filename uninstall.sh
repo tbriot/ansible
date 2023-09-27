80183aeb610d2831be8e95ee3a8cd9656c70aefa
Thomas Briot <thomas.briot82@gmail.com>
2023-09-26 21:07:18 -0400

    Add core setup packages

--- uninstall.sh
+++ uninstall.sh
@@ -6,7 +6,13 @@ apt purge zsh
 rm -fr ~/powerlevel10k
 
 # Delete dotfiles
-rm -f $HOME/.gitconfig
+rm -f ~/.gitconfig
 rm -fr $XDG_CONFIG_HOME/git
 rm -fr $XDG_CONFIG_HOME/p10k
 rm -fr $XDG_CONFIG_HOME/zsh
+
+# Delete SSH directory
+rm -fr ~/.ssh
+
+# Delete packages
+sudo apt -y purge tmux
