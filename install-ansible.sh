882979d054e8faee4322823301c52023ea8b88b9
Thomas Briot <thomas.briot82@gmail.com>
2023-10-09 14:50:33 -0400

    Fix autoapprove flag in apt install command

--- install-ansible.sh
+++ install-ansible.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 sudo apt update
-sudo apt install -y software-properties-common
+sudo apt -y install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt install -y ansible
+sudo apt -y install ansible
